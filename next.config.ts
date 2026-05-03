429c3045f2bd7e3d86b90740926a2e945ffce303
chaitanya <chaitanyagupta.gamini@gmail.com>
2026-05-03 20:53:09 +0530

    sdfsdfsd

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-    /* config options here */
+  /* config options here */
 };
 
 export default nextConfig;
